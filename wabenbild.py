e4fbfc8d3d029273664fcae8d2fd24fbc48a0e0b
Adrian <adrian.steyer@posteo.de>
2024-01-06 02:21:49 +0100

    first code

--- wabenbild.py
+++ wabenbild.py
@@ -6,3 +6,22 @@ e = [1,2,3,4,5,6]
 f = [1,6,5,4,3,2]
 g = [1,3,5,2,4,6]
 
+waben = [a,b,c,d,e,f,g]
+
+def getSingeSideMatch(static1, waben):
+    pass
+
+def getDoubleSideMatch(static1, static2, m1, waben):
+    pass
+
+def getTripleSideMatch(static1, static2, static3, m1, m2, waben):
+    pass
+
+
+for wabe in waben:
+    #get each wabe index that matches wabe[0]
+    wabenOhneSelbst = waben.copy()
+    wabenOhneSelbst.remove(wabe)
+    for andereWabe in wabenOhneSelbst:
+        #do stuff
+        pass
